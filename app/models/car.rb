78435fd5772b87aca0d41dd3777e3c7655e96e72
Yan Wang <Yan@Flatirons-Air-4.fios-router.home>
2018-08-11 17:50:25 -0400

    done

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,11 +1,30 @@
 class Car
 
-  attr_reader :make, :model
+  attr_reader :make, :model, :classification, :owner
 
-  def initialize(make, model, classification)
+  @@all = []
+
+  def initialize(make, model, classification, owner)
     @make = make
     @model = model
     @classification = classification
+    @owner = owner
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
+
+  def self.all_classifications
+    self.all.map do |car|
+      car.classification
+    end
+  end
+
+  def my_mechanics
+    Mechanic.all.select do |mechanic|
+      mechanic.specialty == self.classification
+    end
+  end
 end
--- app/models/mechanic.rb
+++ app/models/mechanic.rb
@@ -2,10 +2,33 @@ class Mechanic
 
   attr_reader :name, :specialty
 
-def initialize(name, specialty)
-  @name = name
-  @specialty = specialty
-end
+  @@all = []
 
+  def initialize(name, specialty)
+    @name = name
+    @specialty = specialty
+    @@all << self
+  end
 
+  def self.all
+    @@all
+  end
+
+  def my_cars
+    Car.all.select do |car|
+      car.classification == self.specialty
+    end
+  end
+
+  def my_owners
+    my_cars.map do |car|
+      car.owner
+    end
+  end
+
+  def my_owner_names
+    my_owners.map do |owner|
+      owner.name
+    end
+  end
 end
