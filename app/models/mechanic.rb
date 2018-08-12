5aa0c0dcf08cd039b2637f5e5196eeba54684257
kristinatong <kristina.tong@gmail.com>
2018-08-12 15:29:10 -0400

    answer

--- app/models/mechanic.rb
+++ app/models/mechanic.rb
@@ -1,11 +1,34 @@
 class Mechanic
+  @@all = []
 
   attr_reader :name, :specialty
 
-def initialize(name, specialty)
-  @name = name
-  @specialty = specialty
-end
+  def initialize(name, specialty)
+    @name = name
+    @specialty = specialty
+    @@all << self
+  end
 
+  def self.all
+    @@all
+  end
+
+  def cars
+    Cars.all.select do |car|
+      car.mechanic == self
+    end
+  end
+
+  def car_owners
+    cars.map do |car|
+      car.owner
+    end.uniq
+  end
+
+  def car_owner_names
+    car_owners.map do |owner|
+      owner.name
+    end
+  end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -4,4 +4,10 @@ def reload
   load 'config/environment.rb'
 end
 
+kris = CarOwner.new('kris')
+forrest = CarOwner.new('forrest')
+lila = Mechanic.new('lila','car')
+bob = Mechanic.new('bob','truck')
+car1 = Car.new('toyota', 'a', 'car', kris, lila)
+car2 = Car.new('bmw','b','car',forrest,bob)
 binding.pry
