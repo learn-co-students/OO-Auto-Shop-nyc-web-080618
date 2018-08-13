257d50e0af6a4c8ae55d5e69d7a1fc93ac0c818b
ParkMD7 <parkermdinsmore@gmail.com>
2018-08-12 23:25:26 -0400

    Done.

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,11 +1,40 @@
 class Car
 
-  attr_reader :make, :model
+attr_reader :make, :model, :classification, :owner, :mechanic
 
-  def initialize(make, model, classification)
+@@all = []
+
+  def initialize(make, model, classification, owner, mechanic)
     @make = make
     @model = model
     @classification = classification
+    @owner = owner
+    @mechanic = mechanic
+    @@all << self
   end
 
+def self.all
+  @@all
 end
+
+
+# Get a list of all car classifications
+def self.classifications
+  class_arr = Car.all.map do |car_ob|
+    #binding.pry
+    car_ob.classification
+  end
+  class_arr
+end # end of car classifications Class Method
+
+
+# Get a list of mechanics that have an
+# expertise that matches the car classification
+def mechanics
+  Mechanic.all.select do |mechanic_ob|
+    #binding.pry
+    mechanic_ob.specialty == self.classification
+  end
+end # end of mechanics instance method
+
+end # end of Car Class
--- app/models/mechanic.rb
+++ app/models/mechanic.rb
@@ -1,11 +1,51 @@
 class Mechanic
 
-  attr_reader :name, :specialty
+attr_reader :name, :specialty
+
+@@all = []
 
 def initialize(name, specialty)
   @name = name
   @specialty = specialty
+  @@all << self
+end
+
+def self.all
+  @@all
 end
 
 
-end
+# Get a list of all cars that a mechanic services
+def cars
+  car_arr = Car.all.select do |car_ob|
+    #binding.pry
+      car_ob.mechanic == self
+  end
+  car_arr.map do |car_ob|
+    car_ob
+  end
+end # end of cars instance method
+
+# Get a list of all the car owners that go to a specific mechanic
+def car_owners
+  car_owners_arr = Car.all.select do |car_ob|
+    #binding.pry
+      car_ob.mechanic == self
+  end
+  car_owners_arr.map do |car_ob|
+    car_ob.owner
+  end
+end # end of car_owners instance method
+
+# Get a list of the names of all car owners who go to a specific mechanic
+def car_owner_names
+  car_owners_arr = Car.all.select do |car_ob|
+    #binding.pry
+      car_ob.mechanic == self
+  end
+  car_owners_arr.map do |car_ob|
+    car_ob.owner.name
+  end
+end # end of car_owner_names
+
+end # end of Mechanic Class
