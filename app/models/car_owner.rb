09cdbf86ace7480496d5023cc4d260a99cc97af1
github username <github email address>
2018-08-18 13:02:27 -0400

    Done.

--- app/models/car_owner.rb
+++ app/models/car_owner.rb
@@ -2,9 +2,34 @@ class CarOwner
 
   attr_reader :name
 
+  @@all = []
+
   def initialize(name)
     @name = name
+    @@all < self
   end
+
+  def self.all
+    @@all
+  end
+
+  def my_cars
+    Cars.all.select do |car|
+      car.owner == self
+    end
+  end
+
+  def my_mechanics
+    my_cars.collect do |car|
+      car.mechanic
+    end
+  end
+
+  def self.avg_cars
+    Car.all.length.to_f / all.length
+  end
+
+
 
 
 end
--- app/models/mechanic.rb
+++ app/models/mechanic.rb
@@ -2,10 +2,35 @@ class Mechanic
 
   attr_reader :name, :specialty
 
-def initialize(name, specialty)
-  @name = name
-  @specialty = specialty
-end
+  @@all = []
+
+  def initialize(name, specialty)
+    @name = name
+    @specialty = specialty
+    @@all < self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def my_cars
+    Car.all.select do |car|
+      car.mechanic == self
+    end
+  end
+
+  def my_clients
+    my_cars.collect do |car|
+      car.owner
+    end.uniq
+  end
+
+  def my_clients_names
+    my_clients.collect do |client|
+      client.name
+    end
+  end
 
 
 end
